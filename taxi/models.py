3449e7ca97d1fa8a3f882f0440fc107b01260c8f
JustVov4uk <volodabudzan4@gmail.com>
2025-08-24 15:48:03 +0200

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -16,7 +16,6 @@ class Manufacturer(models.Model):
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
 
     class Meta:
         ordering = ("username",)
